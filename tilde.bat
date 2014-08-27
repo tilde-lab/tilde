d623ba0bf74a4e258150165a7de4c9170706d782
Evgeny Blokhin <eb@tilde.pro>
2014-08-27 21:43:31 +0200

    Cleaning (not finished)

--- tilde.bat
+++
@@ -1,7 +0,0 @@
-:: Tilde project: entry point for Windows environment
-:: See http://tilde.pro
-
-@echo off
-set CUR=%~dp0
-"%CUR%\\windows\\python\\python.exe" "%CUR%\\core\\cli.py" %*
-pause > nul
--- windows/spglib/__init__.py
+++
@@ -1,2 +0,0 @@
-# Workaround to provide Windows compatibility
-sys.path.append(os.path.realpath(os.path.dirname(__file__) + '/python'))
